34dc0df63fab94863379f57632a864da35689473
Sebastian Muresan <sebastian16m16@gmail.com>
2019-03-08 14:58:51 +0200

    Formatted app class

--- src/main/java/ro/utcn/pt/lab01/App.java
+++ src/main/java/ro/utcn/pt/lab01/App.java
@@ -1,24 +1,22 @@
 package ro.utcn.pt.lab01;
+
 import java.util.*;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Polynome p1 = new Polynome();
-        Polynome p2 = new Polynome();
-        
-        p1.insert(-10, 2);
-        p1.insert(-4, 6);
-        p1.insert(9, 0);
-        
-        p2.insert(15, 10);
-        p2.insert(5, 3);
-        p2.insert(9, 0);
-        p2.insert(5, 1);
-        
-        
-        p1.printPoly();
-    	p2.printPoly();
-    }
+public class App {
+	public static void main(String[] args) {
+		Polynome p1 = new Polynome();
+		Polynome p2 = new Polynome();
+
+		p1.insert(-10, 2);
+		p1.insert(-4, 6);
+		p1.insert(9, 0);
+
+		p2.insert(15, 10);
+		p2.insert(5, 3);
+		p2.insert(9, 0);
+		p2.insert(5, 1);
+
+		p1.printPoly();
+		p2.printPoly();
+	}
 }
