17febf4142b8aaf13b8b016146b1a1835a773aa5
Sebastian Muresan <sebastian16m16@gmail.com>
2019-03-08 14:59:40 +0200

    1

--- src/main/java/ro/utcn/pt/lab01/Polynome.java
+++ src/main/java/ro/utcn/pt/lab01/Polynome.java
@@ -1,56 +1,55 @@
 package ro.utcn.pt.lab01;
 
 public class Polynome {
-	
+
 	Monom head;
-	
+
 	public void insert(int coef, int degree) {
-		
+
 		Monom monom = new Monom();
 		monom.coef = coef;
 		monom.degree = degree;
 		monom.next = null;
-		
-		if(head == null) {
+
+		if (head == null) {
 			head = monom;
-		}else {
-			
+		} else {
+
 			Monom m = head;
-			
-			while(m.next != null) {
-				
+
+			while (m.next != null) {
+
 				m = m.next;
 			}
 			m.next = monom;
-			
+
 		}
 	}
-	
-	public int getPolySize() { 
-     
-        Monom temp = head; 
-        int size = 0; 
-        while (temp != null) 
-        { 
-            size++; 
-            temp = temp.next; 
-        } 
-        return size; 
-    } 
-	
+
+	public int getPolySize() {
+
+		Monom temp = head;
+		int size = 0;
+		while (temp != null) {
+			size++;
+			temp = temp.next;
+		}
+		return size;
+	}
+
 	public void arrangePoly() {
 		int x, y;
-		for(int i=0; i<getPolySize()/2; i++) {
+		for (int i = 0; i < getPolySize() / 2; i++) {
 			Monom m = head;
-			
-			while(m.next != null) {
-				if(m.degree < m.next.degree && m.next != null) {
-					
-					//switch degrees
+
+			while (m.next != null) {
+				if (m.degree < m.next.degree && m.next != null) {
+
+					// switch degrees
 					x = m.degree;
 					m.degree = m.next.degree;
 					m.next.degree = x;
-					//switch coefs
+					// switch coefs
 					y = m.coef;
 					m.coef = m.next.coef;
 					m.next.coef = y;
@@ -59,63 +58,60 @@ public class Polynome {
 			}
 		}
 	}
-	
+
 	public void printPoly() {
 
 		arrangePoly();
-		
+
 		Monom m = head;
-		
-		while(m.next != null) {
-			
-			if(m.degree == 0){
-				if(m.coef < 0) {
+
+		while (m.next != null) {
+
+			if (m.degree == 0) {
+				if (m.coef < 0) {
 					System.out.print(m.coef);
-				}else {
+				} else {
 					System.out.print("+" + m.coef);
 				}
-			}else if(m.degree == 1){
-				if(m.coef < 0) {
+			} else if (m.degree == 1) {
+				if (m.coef < 0) {
 					System.out.print(m.coef + "x");
-				}else {
+				} else {
 					System.out.print("+" + m.coef + "x");
 				}
-			}else{
-				if(m.coef < 0) {
+			} else {
+				if (m.coef < 0) {
 					System.out.print(m.coef + "x^" + m.degree);
-				}else {
+				} else {
 					System.out.print("+" + m.coef + "x^" + m.degree);
 				}
 			}
-			
-			
+
 			m = m.next;
 		}
-		
-		if(m.degree == 0){
-			if(m.coef < 0) {
+
+		if (m.degree == 0) {
+			if (m.coef < 0) {
 				System.out.print(m.coef);
-			}else {
+			} else {
 				System.out.print("+" + m.coef);
 			}
-		}else if(m.degree == 1){
-			if(m.coef < 0) {
+		} else if (m.degree == 1) {
+			if (m.coef < 0) {
 				System.out.print(m.coef + "x");
-			}else {
+			} else {
 				System.out.print("+" + m.coef + "x");
 			}
-		}else{
-			if(m.coef < 0) {
+		} else {
+			if (m.coef < 0) {
 				System.out.print(m.coef + "x^" + m.degree);
-			}else {
+			} else {
 				System.out.print("+" + m.coef + "x^" + m.degree);
 			}
 		}
-		
+
 		System.out.println();
 
-	
 	}
-	
-	
+
 }
